1f1b1df585c7a71a2dc51faf80b3a357f039cb9c
SergeZoghbi <serge2zoghbi1@gmail.com>
2019-12-30 22:44:26 +0200

    first commit

---
+++ Presentation/Login/Login.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en" style="min-height: 100% !important;
+    height: 100%;">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="HTML5 Template"/>
+    <meta name="description" content="SEOhub - SEO, Marketing. Social Media, Multipurpose HTML5 Template"/>
+    <meta name="author" content="potenzaglobalsolutions.com"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <title>SEOhub - SEO, Marketing. Social Media, Multipurpose HTML5 Template</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/images/favicon.ico"/>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" type="text/css"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/bootstrap.min.css"/>
+
+    <!-- mega menu -->
+    <link rel="stylesheet" type="text/css"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/mega-menu/mega_menu.css"/>
+
+    <!-- font awesome -->
+    <link rel="stylesheet" type="text/css"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/font-awesome.min.css"/>
+
+    <!-- Themify icons -->
+    <link rel="stylesheet" type="text/css"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/themify-icons.css"/>
+
+    <!-- owl-carousel -->
+    <link rel="stylesheet" type="text/css"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/owl-carousel/owl.carousel.css"/>
+
+    <!-- magnific-popup -->
+    <link rel="stylesheet" type="text/css"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/magnific-popup/magnific-popup.css"/>
+
+    <!-- main style -->
+    <link rel="stylesheet" type="text/css"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/style.css"/>
+
+    <!-- Responsive style -->
+    <link rel="stylesheet" type="text/css"
+          href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/responsive.css"/>
+
+
+</head>
+
+<body>
+<header id="header" class="default">
+    <div class="menu">
+        <nav id="menu" class="mega-menu">
+            <section class="menu-list-items">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- menu logo -->
+                            <ul class="menu-logo">
+                                <li>
+                                    <a><img id="logo_img"
+                                            src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/images/logo-dark.png"
+                                            alt="logo"> </a>
+                                </li>
+                            </ul>
+                            <ul class="menu-links">
+                                <li><a href="javascript:void(0)">Shop</a></li>
+                                <li><a href="javascript:void(0)">Rifles</a></li>
+                                <li><a href="javascript:void(0)">SMG</a></li>
+                                <li><a href="javascript:void(0)">Pistols</a></li>
+                                <li><a href="javascript:void(0)">Heavy</a></li>
+                                <li><a href="./Login.php">Login</a></li>
+                                <li><a href="./Register.php">Register</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </nav>
+    </div>
+</header>
+
+<!--=================================
+header -->
+
+<!--=================================
+register-form  -->
+<section class="login-form page-section-ptb">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+                <h4>Login To Your Account</h4>
+                <form class="gray-form mt-2 clearfix">
+                    <div class="form-group">
+                        <label for="name">User name* </label>
+                        <input id="name" class="form-control" type="text" placeholder="User name" name="web">
+                    </div>
+                    <div class="form-group">
+                        <label for="Password">Password* </label>
+                        <input id="Password" class="form-control" type="password" placeholder="Password"
+                               name="Password">
+                    </div>
+                    <a href="#" class="button"> Log in </a>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--=================================
+register-form  -->
+
+<!--=================================
+footer -->
+
+<footer class="footer footer-topbar page-section-pt">
+    <div class="container">
+        <div class="col-lg-3 col-md-2">
+            <img class="img-fluid"
+                 src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/images/logo-footer.png"
+                 alt="">
+        </div>
+
+        <div class="copyright mt-6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul class="list-inline text-left">
+                            <li><a href="#">Terms & Conditions </a> &nbsp;&nbsp;&nbsp;|</li>
+                            <li><a href="#">API Use Policy </a> &nbsp;&nbsp;&nbsp;|</li>
+                            <li><a href="#">Privacy Policy </a> &nbsp;&nbsp;&nbsp;|</li>
+                            <li><a href="#">Cookies </a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-right">
+                            <p>Copyright © 2020 SEO Solution. All Rights Reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
+
+<!--=================================
+footer -->
+
+<!-- jquery  -->
+<script type="text/javascript"
+        src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/jquery.min.js"></script>
+<script type="text/javascript"
+        src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/popper.min.js"></script>
+
+<!-- bootstrap -->
+<script type="text/javascript"
+        src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/bootstrap.min.js"></script>
+
+<!-- mega-menu -->
+<script type="text/javascript"
+        src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/mega-menu/mega_menu.js"></script>
+
+
+<!-- custom -->
+<script type="text/javascript"
+        src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/custom.js"></script>
+
+
+</body>
+
+</html>
---
+++ Presentation/Login/Register.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="HTML5 Template" />
+    <meta name="description" content="SEOhub - SEO, Marketing. Social Media, Multipurpose HTML5 Template" />
+    <meta name="author" content="potenzaglobalsolutions.com" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>SEOhub - SEO, Marketing. Social Media, Multipurpose HTML5 Template</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/images/favicon.ico" />
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" type="text/css" href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/bootstrap.min.css" />
+
+    <!-- mega menu -->
+    <link rel="stylesheet" type="text/css" href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/mega-menu/mega_menu.css" />
+
+    <!-- font awesome -->
+    <link rel="stylesheet" type="text/css" href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/font-awesome.min.css" />
+
+    <!-- Themify icons -->
+    <link rel="stylesheet" type="text/css" href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/themify-icons.css" />
+
+    <!-- main style -->
+    <link rel="stylesheet" type="text/css" href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/style.css" />
+
+    <!-- Responsive style -->
+    <link rel="stylesheet" type="text/css" href="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/css/responsive.css" />
+
+
+
+
+</head>
+
+<body>
+
+<header id="header" class="default">
+    <div class="menu">
+        <nav id="menu" class="mega-menu">
+            <section class="menu-list-items">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- menu logo -->
+                            <ul class="menu-logo">
+                                <li>
+                                    <a><img id="logo_img"
+                                            src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/images/logo-dark.png"
+                                            alt="logo"> </a>
+                                </li>
+                            </ul>
+                            <ul class="menu-links">
+                                <li><a href="javascript:void(0)">Shop</a></li>
+                                <li><a href="javascript:void(0)">Rifles</a></li>
+                                <li><a href="javascript:void(0)">SMG</a></li>
+                                <li><a href="javascript:void(0)">Pistols</a></li>
+                                <li><a href="javascript:void(0)">Heavy</a></li>
+                                <li><a href="./Login.php">Login</a></li>
+                                <li><a href="./Register.php">Register</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </nav>
+    </div>
+</header>
+
+<section class="login-form page-section-ptb">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="mb-3">Register An Account</h4>
+                <form class="gray-form">
+                    <div class="row">
+                        <div class="form-group col-md-12">
+                            <label for="name">User Name* </label>
+                            <input class="form-control" type="text" placeholder="Choose your user name" name="web">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="Password">Password* </label>
+                            <input class="form-control" type="password" placeholder="Password" name="Password">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="Password">Re-enter Password*</label>
+                            <input class="form-control" type="password" placeholder="Password" name="Password">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="email">Email *</label>
+                            <input class="form-control" type="text" placeholder="Enter your email" name="email">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="phone">Mobile phone *</label>
+                            <input id="phone" class="form-control" type="text" placeholder="Enter your mobile no" name="phone">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<!--=================================
+register-form  -->
+
+<!--=================================
+footer -->
+
+<footer class="footer footer-topbar page-section-pt">
+    <div class="container">
+        <div class="col-lg-3 col-md-2">
+            <img class="img-fluid"
+                 src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/images/logo-footer.png"
+                 alt="">
+        </div>
+
+        <div class="copyright mt-6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul class="list-inline text-left">
+                            <li><a href="#">Terms & Conditions </a> &nbsp;&nbsp;&nbsp;|</li>
+                            <li><a href="#">API Use Policy </a> &nbsp;&nbsp;&nbsp;|</li>
+                            <li><a href="#">Privacy Policy </a> &nbsp;&nbsp;&nbsp;|</li>
+                            <li><a href="#">Cookies </a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-right">
+                            <p>Copyright © 2020 SEO Solution. All Rights Reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
+<!--=================================
+footer -->
+
+
+<!-- jquery  -->
+<script type="text/javascript" src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/jquery.min.js"></script>
+<script type="text/javascript" src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/popper.min.js"></script>
+
+<!-- bootstrap -->
+<script type="text/javascript" src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/bootstrap.min.js"></script>
+
+<!-- mega-menu -->
+<script type="text/javascript" src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/mega-menu/mega_menu.js"></script>
+
+<!-- select -->
+<script type="text/javascript" src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/select/jquery-select.js"></script>
+
+<!-- custom -->
+<script type="text/javascript" src="../../seohub%20seo%20marketing%20social%20media%20multipurpose%20html5/Template/js/custom.js"></script>
+
+
+
+</body>
+
+</html>
